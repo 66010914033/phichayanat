01f42105d44d6834cd7ff6d054719eed7252a290
Phichayanat <66010914033@msu.ac.th>
2026-02-17 21:13:51 +0700

    i

--- i/delete_province.php
+++ i/delete_province.php
@@ -1,12 +1,29 @@
-<meta charset="utf-8">
 <?php
+include_once("connectdb.php");
 
-$id = $_GET['id'];
-include 'connectdb.php';
-$sql = "DELETE FROM province WHERE p_id = '{$id}'";
-mysqli_query($conn, $sql) or die ("ลบข้อมูลไม่ได้");
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    
+    // ค้นหาข้อมูลเพื่อลบรูปภาพ
+    $sql_find = "SELECT * FROM provinces WHERE p_id = '$id'";
+    $rs_find = mysqli_query($conn, $sql_find);
+    $data = mysqli_fetch_array($rs_find);
+    
+    if($data){
+        $image_file = "img/" . $data['p_id'] . "." . $data['p_ext'];
+        if(file_exists($image_file)){ unlink($image_file); }
 
-echo "<script>";
-echo "window.location='b.php';";
-echo "</script>";
+        $sql_del = "DELETE FROM provinces WHERE p_id = '$id'";
+        
+        if(mysqli_query($conn, $sql_del)){
+            // แก้ให้เด้งกลับไปหน้า b.php
+            echo "<script>alert('ลบเรียบร้อย'); window.location='b.php';</script>";
+        } else {
+            echo "Error: " . mysqli_error($conn);
+        }
+    } else {
+        echo "<script>alert('ไม่พบข้อมูล'); window.location='b.php';</script>";
+    }
+}
+mysqli_close($conn);
 ?>
