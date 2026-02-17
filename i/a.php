4114ee3f16c74cbd5c451a9c8f2499c629ecbd2e
Phichayanat <66010914033@msu.ac.th>
2026-02-17 18:17:57 +0700

    i

--- i/a.php
+++ i/a.php
@@ -6,6 +6,17 @@
 </head>
 
 <body>
-    <h1>งาน i  -- พิชญาณัฏฐ์	 รินทร์วงค์<h/1>
+<h1>งาน i  -- พิชญาณัฏฐ์	 รินทร์วงค์<h/1>
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM `regions`";
+$rs = mysqli_query($conn,$sql);
+while ($data = mysqli_fetch_array($rs)){
+    echo $data['r_id']."<br>";
+    echo $data['r_name']."<hr>";
+
+}
+mysqli_close($conn);
+?>
 </body>
 </html>
