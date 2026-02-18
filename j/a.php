26ba0b3f4a82dad9402791846083ea9bf96f59fe
Phichayanat <66010914033@msu.ac.th>
2026-02-18 13:25:31 +0700

    1

---
+++ j/a.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>พิชญาณัฏฐ์ รินทร์วงค์ (อินเตอร์)</title>
+</head>
+
+<body>
+<h1>66010914033 พิชญาณัฏฐ์ รินทร์วงค์ (อินเตอร์)</h1>
+
+
+<img src="1.jfif" width="400">
+
+
+
+</body>
+</html>
